03ee37a5a5f66c3405b58d41c3afb6c63a58ecdb
= <=>
2023-07-09 23:25:43 -0400

    comprehension complted

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == 0]
+    return even_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaim = [str + "!" for str in sentence_list]
+    return exclaim
